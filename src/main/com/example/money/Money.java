fb16e78d9c0130f4b689cca045c212e05c3ad4dd
Alex Calovi <alex.calovi98@gmail.com>
2022-11-13 14:53:54 +0100

    added money class using TDD approach step by step

---
+++ src/main/com/example/money/Money.java
@@ -0,0 +1,34 @@
+package com.example.money;
+
+public class Money {
+
+    private final int amount;
+    private final String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount && currency.equals(money.currency);
+    }
+
+    public String currency() {
+        return currency;
+    }
+}
